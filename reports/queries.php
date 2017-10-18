cd9f1d484ff787ac5aa89d24f9ce35a7947cc41d
Tim Ramsier <tim.ramsier@dictionary.com>
2017-10-17 21:13:54 -0700

    htaccess files and report fix

--- reports/queries.php
+++ reports/queries.php
@@ -1,13 +1,13 @@
 <?php
 $queryList = array(
-  "main"=>"SELECT 'Access Key','Attending','First Name','Last Name','Company','Title','Email','Address 1','Address 2','City','State','Zip',
+  "main"=>"SELECT 'Access Key','Attending','First Name','Last Name','Company','Title','Email','Address 1','Address 2','Country','City','State','Zip',
             'Office Phone','Mobile Phone','EA Name','EA Email','EA Office Phone','EA Mobile Phone','Emergency Contact Name',
             'Emergency Phone','Food and Diet','ADA and Special Needs','Jacket Size','Arrival Date',
             'Arrival Airport','Arrival Flight Number','Arrival Airline','Origin City','Arrival Connection Info','Departure Date',
             'Departure Airport','Departure Flight Number','Departure Airline','Destination City','Departure Connection Info'
             FROM data
             UNION
-            SELECT  hashKey,attending,firstName,lastName,company,title,email,address1,address2,city,state,zip,
+            SELECT  hashKey,attending,firstName,lastName,company,title,email,address1,address2,country,city,state,zip,
             officePhone,mobilePhone,eaName,eaEmail,eaOfficePhone,eaMobilePhone,emergencyName,
             emergencyPhone,foodDiet,specialNeeds,jacketSize,arrivalDate,
             arrivalAirport,arrivalFlightNum,arrivalAirline,originCity,arrivalConnectionInfo,departureDate,
